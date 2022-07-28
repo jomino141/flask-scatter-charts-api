cabc5ee3e056a1628b5ae65c4b4cf2626dda70f7
leonardKleber <leonard.kleber@outlook.com>
2022-07-28 22:42:14 +0200

    Functionality to create 2d charts

--- server/app.py
+++ server/app.py
@@ -1,1 +1,21 @@
+from flask import Flask
+from charts import *
 
+
+app = Flask(__name__)
+
+
+# Define API's index route.
+@app.route('/')
+def index():
+	return 'API for generating scatter ApexCharts.'
+
+
+# Creates a JSON for a 2D scatter chart containing data points.
+@app.route('/2d/<parameter1>/<parameter2>')
+def create_2d_chart(parameter1, parameter2):
+    return create_2d_scatter(int(parameter1), int(parameter2))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ server/charts.py
@@ -0,0 +1,57 @@
+import json
+
+
+# Read array from local example JSON file.
+with open('example.json', 'r') as file:
+	example_data = json.load(file)
+
+
+# Gets an array of all values of a single parameter.
+def get_values(parameter):
+	values = []
+
+	for i in example_data:
+		values.append(i[0][parameter])
+
+	return values
+
+
+# Creates an array of data points with all values of two parameters.
+def create_data_points(parameter1, parameter2):
+	values1 = get_values(parameter1)
+	values2 = get_values(parameter2)
+
+	data_points = []
+
+	for i in range(len(values1)):
+		data_point = [values1[i], values2[i]]
+
+		data_points.append(data_point)
+
+	return data_points
+
+
+# Creates a JSON string for a 2D scatter chart containing data points.
+def create_2d_scatter(parameter1, parameter2):
+	data_points = create_data_points(parameter1, parameter2)
+
+	json_string = {
+		'series': [
+			{'name': 'SAMPLE', 
+			'data': data_points},
+		],
+		'options': {
+			'chart': {
+				'height': 350, 
+				'type': 'scatter', 
+				'zoom': {
+					'enabled': True, 
+					'type': 'xy'
+				}
+			},
+			'xaxis': {'tickAmount': 10,},
+			'yaxis': {'tickAmount': 7}
+		}
+	}
+
+	return json_string
