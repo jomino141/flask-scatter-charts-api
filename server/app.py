43bc0475fb5a7a3510013f812fc8e6a62d4702da
Leonard Kleber <83468990+leonardKleber@users.noreply.github.com>
2022-08-02 01:27:45 +0200

    Update app.py

--- server/app.py
+++ server/app.py
@@ -14,7 +14,13 @@ def index():
 # Creates a JSON for a 2D scatter chart containing data points.
 @app.route('/2d/<parameter1>/<parameter2>')
 def create_2d_chart(parameter1, parameter2):
-    return create_2d_scatter(int(parameter1), int(parameter2))
+    return create_2d_json(int(parameter1), int(parameter2))
+
+
+# Creates a JSON for a 3D scatter chart containing data points.
+@app.route('/3d/<parameter1>/<parameter2>/<parameter3>')
+def create_3d_chart(parameter1, parameter2, parameter3):
+    return create_3d_json(int(parameter1), int(parameter2), int(parameter3))
 
 
 if __name__ == '__main__':
